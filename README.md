9d26f5330903c87b7032d34663b1887c7cb2670d
Rajdeep Bharati <rajdeep@macports.org>
2020-07-11 18:05:12 +0530

    Add endpoints for adaptive bitrate streaming

--- README.md
+++ README.md
@@ -19,11 +19,17 @@ go build .
 
 ## Approach
 
+### Single (original) resolution:
+
 - Get mp4 url from user, check its resolution using ffprobe.
 - generate a uuid, which is used as the folder name for a particular stream.
 - all streams are stored in the `streams` folder in the server.
 - ffmpeg commands are invoked from golang. (It's better to use some bindings, but I didn't use it right now due to time constraints)
 - If the stream is successfully created, the id/url is sent back to the user.
+
+### Adaptive bitrate (360p,480p,720p,1080p):
+
+- Didn't check resolution of original file, and followed the same steps as above.
 
 ## Demo & Deployment
 
@@ -32,6 +38,13 @@ go build .
 **demo video**: https://youtu.be/zQEBMOA1bQU
 
 **sample stream**: http://161.35.56.22/streams/f4a88d14-f5b9-43c1-b684-d346da76a93d/f4a88d14-f5b9-43c1-b684-d346da76a93d.m3u8
+
+**ABR streams**:
+
+- http://161.35.56.22/streamsabr/e2466dff-05ba-41fd-8b4a-66e0d747954e/360p.m3u8
+- http://161.35.56.22/streamsabr/e2466dff-05ba-41fd-8b4a-66e0d747954e/480p.m3u8
+- http://161.35.56.22/streamsabr/e2466dff-05ba-41fd-8b4a-66e0d747954e/720p.m3u8
+- http://161.35.56.22/streamsabr/e2466dff-05ba-41fd-8b4a-66e0d747954e/1080p.m3u8
 
 ## API Endpoints
 
@@ -50,8 +63,8 @@ Sample response:
 
 ```json
 {
-    "Id": "2cfe0e40-61f9-4287-b136-4fab49b597e3",
-    "Url": "http://161.35.56.22/streams/2cfe0e40-61f9-4287-b136-4fab49b597e3/2cfe0e40-61f9-4287-b136-4fab49b597e3.m3u8"
+  "Id": "2cfe0e40-61f9-4287-b136-4fab49b597e3",
+  "Url": "http://161.35.56.22/streams/2cfe0e40-61f9-4287-b136-4fab49b597e3/2cfe0e40-61f9-4287-b136-4fab49b597e3.m3u8"
 }
 ```
 
@@ -60,3 +73,27 @@ Sample response:
 Use VLC media player or safari browser to stream:
 
 Sample url: http://161.35.56.22/streams/f4a88d14-f5b9-43c1-b684-d346da76a93d/f4a88d14-f5b9-43c1-b684-d346da76a93d.m3u8
+
+### `POST /submitabr/`
+
+```bash
+curl http://localhost:80/submitabr/ --data "input=https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4"
+```
+
+Sample response:
+
+```json
+{
+  "Id": "38a287de-bcbc-44d4-84d0-731188de1630",
+  "Url360p": "http://localhost/streamsabr/38a287de-bcbc-44d4-84d0-731188de1630/360p.m3u8",
+  "Url480p": "http://localhost/streamsabr/38a287de-bcbc-44d4-84d0-731188de1630/480p.m3u8",
+  "Url720p": "http://localhost/streamsabr/38a287de-bcbc-44d4-84d0-731188de1630/720p.m3u8",
+  "Url1080p": "http://localhost/streamsabr/38a287de-bcbc-44d4-84d0-731188de1630/1080p.m3u8"
+}
+```
+
+### `GET /streamsabr/<id>/<resolution>.m3u8`
+
+Use VLC media player or safari browser to stream:
+
+Sample url: http://161.35.56.22/streamsabr/e2466dff-05ba-41fd-8b4a-66e0d747954e/1080p.m3u8
--- main.go
+++ main.go
@@ -19,6 +19,13 @@ type StreamResponse struct {
 	Id  string
 	Url string
 }
+type StreamResponseABR struct {
+	Id       string
+	Url360p  string
+	Url480p  string
+	Url720p  string
+	Url1080p string
+}
 
 func main() {
 	path, err := os.Getwd()
@@ -30,6 +37,7 @@ func main() {
 	// Don't allow accessing /streams/ route
 	// (only allow `/streams/:uuid/` or `/streams/:uuid/:uuid.m3u8`)
 	http.HandleFunc("/streams/", func(w http.ResponseWriter, r *http.Request) {
+		// serves the stream of original resolution
 		if r.URL.Path != "/streams/" {
 			println(path + " now " + r.URL.Path)
 			http.ServeFile(w, r, r.URL.Path[1:])
@@ -38,6 +46,75 @@ func main() {
 			http.NotFound(w, r)
 			return
 		}
+	})
+
+	http.HandleFunc("/streamsabr/", func(w http.ResponseWriter, r *http.Request) {
+		// serves the stream of specified resolution
+		if r.URL.Path != "/streamsabr/" {
+			println(path + " now " + r.URL.Path)
+			http.ServeFile(w, r, r.URL.Path[1:])
+		} else {
+			println("no id provided!")
+			http.NotFound(w, r)
+			return
+		}
+	})
+
+	http.HandleFunc("/submitabr/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/submitabr/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		if r.Method == "GET" {
+			fmt.Fprintf(w, "GET, %q", html.EscapeString(r.URL.Path))
+		} else if r.Method == "POST" {
+			println(r.FormValue("input"))
+			id := guuid.New()
+			cmd1 := exec.Command("mkdir", "streamsabr/"+id.String())
+			cmd1.Output()
+			outputPath := "./streamsabr/" + id.String()
+			urlinput := r.FormValue("input")
+			println("outpath", outputPath)
+
+			cmda := exec.Command("ffmpeg", "-i", urlinput, "-profile:v", "baseline", "-level", "3.0", "-s", "640x360", "-start_number", "0", "-hls_time", "10", "-hls_list_size", "0", "-f", "hls", outputPath+"/360p.m3u8")
+			cmda.Output()
+			cmdb := exec.Command("ffmpeg", "-i", urlinput, "-profile:v", "baseline", "-level", "3.0", "-s", "842x480", "-start_number", "0", "-hls_time", "10", "-hls_list_size", "0", "-f", "hls", outputPath+"/480p.m3u8")
+			cmdb.Output()
+			cmdc := exec.Command("ffmpeg", "-i", urlinput, "-profile:v", "baseline", "-level", "3.0", "-s", "1280x720", "-start_number", "0", "-hls_time", "10", "-hls_list_size", "0", "-f", "hls", outputPath+"/720p.m3u8")
+			cmdc.Output()
+			cmdd := exec.Command("ffmpeg", "-i", urlinput, "-profile:v", "baseline", "-level", "3.0", "-s", "1920x1080", "-start_number", "0", "-hls_time", "10", "-hls_list_size", "0", "-f", "hls", outputPath+"/1080p.m3u8")
+			cmdd.Output()
+
+			if err != nil {
+				println(err.Error())
+				if err.Error() == "exit status 1" {
+					fmt.Fprintf(w, "Please use a valid url (which returns a mp4 file)")
+					println("Please use a valid url (which returns a mp4 file)")
+				}
+				return
+			}
+			hostURL := "http://" + r.Host + "/streamsabr/"
+			streamResponseABR := StreamResponseABR{
+				id.String(),
+				hostURL + id.String() + "/360p.m3u8",
+				hostURL + id.String() + "/480p.m3u8",
+				hostURL + id.String() + "/720p.m3u8",
+				hostURL + id.String() + "/1080p.m3u8",
+			}
+			js, err2 := json.Marshal(streamResponseABR)
+			if err2 != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(js)
+
+		} else {
+			http.Error(w, "Invalid request method.", 405)
+		}
+
 	})
 
 	http.HandleFunc("/submit/", func(w http.ResponseWriter, r *http.Request) {
